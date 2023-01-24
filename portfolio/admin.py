cdd56bca7cd801ba78cb41e9a20e042ac449dbd5
shahul hameed <shahul66649@gmail.com>
2023-01-24 03:14:01 -1200

    Delete portfolio directory

--- portfolio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from portfolio.models import contact
-
-# Register your models here.
-
-admin.site.register(contact)
--- portfolio/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from portfolio import views
-
-urlpatterns = [
-    path('',views.index,name="index")
-]
